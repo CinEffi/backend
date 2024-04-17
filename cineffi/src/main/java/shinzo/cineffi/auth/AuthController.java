47514ebd510564ca82024fb58e4b13146de7a71b
Yeji Kim <kimpizzzaa@gmail.com>
2024-04-17 12:51:22 +0900

    feat : 카카오 토큰 받아오기

--- cineffi/src/main/java/shinzo/cineffi/auth/AuthController.java
+++ cineffi/src/main/java/shinzo/cineffi/auth/AuthController.java
@@ -1,11 +1,22 @@
 package shinzo.cineffi.auth;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import shinzo.cineffi.domain.dto.KakaoToken;
 
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @GetMapping("/api/auth/login/kakao")
+    public KakaoToken getKakaoToken(@RequestParam final String code){
+        //인가코드로 엑세스 코드 발급
+        KakaoToken kakaoToken = authService.getAccessToken(code);
+
+        return kakaoToken;
+    }
+
 }
