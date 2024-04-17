c81efa2d4012aa65bf187e89311269c3b823ffc1
Yeji Kim <kimpizzzaa@gmail.com>
2024-04-17 12:51:22 +0900

    feat : 카카오 토큰 받아오기

--- cineffi/src/main/java/shinzo/cineffi/auth/AuthController.java
+++ cineffi/src/main/java/shinzo/cineffi/auth/AuthController.java
@@ -1,5 +1,10 @@
 package shinzo.cineffi.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import shinzo.cineffi.domain.dto.KakaoToken;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +18,17 @@ import shinzo.cineffi.exception.message.SuccessMsg;
 
 @RestController
 public class AuthController {
+    private final AuthService authService;
+
+    @GetMapping("/api/auth/login/kakao")
+    public KakaoToken getKakaoToken(@RequestParam final String code){
+        //인가코드로 엑세스 코드 발급
+        KakaoToken kakaoToken = authService.getAccessToken(code);
+
+        return kakaoToken;
+    }
+
+}
     private final AuthService authService;
 
     @Autowired
--- cineffi/src/main/java/shinzo/cineffi/auth/AuthService.java
+++ cineffi/src/main/java/shinzo/cineffi/auth/AuthService.java
@@ -1,5 +1,22 @@
 package shinzo.cineffi.auth;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import shinzo.cineffi.domain.dto.KakaoToken;
+import shinzo.cineffi.user.repository.UserAccountRepository;
+import shinzo.cineffi.user.repository.UserCoreRepository;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
@@ -16,6 +33,52 @@ import shinzo.cineffi.user.repository.UserRepository;
 @Service
 @ResponseBody
 public class AuthService {
+    private final UserCoreRepository userCoreRepo;
+    private final UserAccountRepository userAccountRepo;
+
+    @Value("${kakao.rest_api_key}")
+    private String restApiKey;
+    @Value("${kakao.redirect_url}")
+    private String redirectUrl;
+
+    public KakaoToken getAccessToken(String code){
+        RestTemplate rt = new RestTemplate();
+        //요청보낼 헤더 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        //요청보낼 바디 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", restApiKey);
+        params.add("redirect_uri", redirectUrl);
+        params.add("code", code);
+        //헤더 바디 합치기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+                new HttpEntity<>(params, headers);
+
+        //ResponseEntity 객체를 String 형만 받도록 생성. 응답받는 값이 Json 형식이니까
+        ResponseEntity<String> accessTokenResponse = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        //String으로 받은 Json 형식의 데이터를 ObjectMapper 라는 클래스를 사용해 객체로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        KakaoToken kakaoToken = null;
+        try {
+            kakaoToken = objectMapper.readValue(accessTokenResponse.getBody(), KakaoToken.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return kakaoToken;
+
+    }
+
+
+
 
     private final UserAccountRepository userAccountRepository;
     private final UserRepository userRepository;
