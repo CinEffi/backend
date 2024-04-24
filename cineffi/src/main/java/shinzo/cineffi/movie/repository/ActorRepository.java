ad133c76480c28016f49e229ef99df0b3ead0189
Yeji Kim <kimpizzzaa@gmail.com>
2024-04-24 19:04:58 +0900

    feat : init 기능 (배우) #12

--- cineffi/src/main/java/shinzo/cineffi/movie/repository/ActorRepository.java
+++ cineffi/src/main/java/shinzo/cineffi/movie/repository/ActorRepository.java
@@ -3,5 +3,9 @@ package shinzo.cineffi.movie.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shinzo.cineffi.domain.entity.movie.Actor;
 
+import java.util.Optional;
+
 public interface ActorRepository extends JpaRepository<Actor, Long> {
+
+    Optional<Actor> findByName(String name);
 }
--- cineffi/src/main/java/shinzo/cineffi/movie/repository/DirectorRepository.java
+++ cineffi/src/main/java/shinzo/cineffi/movie/repository/DirectorRepository.java
@@ -3,5 +3,9 @@ package shinzo.cineffi.movie.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shinzo.cineffi.domain.entity.movie.Director;
 
+import java.util.Optional;
+
 public interface DirectorRepository extends JpaRepository<Director, Long> {
+
+    Optional<Director> findByName(String name);
 }
