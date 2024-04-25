e434613149439f894dd060ed915374c4f7488478
Yeji Kim <kimpizzzaa@gmail.com>
2024-04-25 21:44:29 +0900

    feat: 데이터 init 통신 비동기화 #12

--- cineffi/src/main/java/shinzo/cineffi/movie/MovieController.java
+++ cineffi/src/main/java/shinzo/cineffi/movie/MovieController.java
@@ -1,6 +1,5 @@
 package shinzo.cineffi.movie;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +12,25 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class MovieController {
-
     private final MovieService movieService;
+
+    @GetMapping("/api/movie/init")
+    public ResponseEntity<ResponseDTO<?>> init() {
+        long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
+
+        movieService.fetchTMDBIdsByDate();
+
+        long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
+        long secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
+
+//        movieService.initMovieData();
+        return ResponseEntity.ok(
+                ResponseDTO.builder()
+                        .message(SuccessMsg.SUCCESS.getDetail())
+                        .result(secDiffTime)
+                        .build()
+        );
+    }
 
     @GetMapping("/api/movie/updateBoxOffice")
     public ResponseEntity<ResponseDTO<?>> updateBoxOffice() {
@@ -27,4 +43,5 @@ public class MovieController {
                         .build()
         );
     }
+
 }
