ea38a840bff37df19fb75dd2905e65b11fc9f95d
Jaeyoung Choi <oxcart@Jaeyoungui-MacBookPro.local>
2024-04-29 16:18:37 +0900

    -

--- cineffi/src/main/java/shinzo/cineffi/CineffiApplication.java
+++ cineffi/src/main/java/shinzo/cineffi/CineffiApplication.java
@@ -12,5 +12,15 @@ public class CineffiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CineffiApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
 
 }
