c468d1e56c7ac5772cd317303cfe2dfdbca909eb
siontext <topaegr3525@kakao.com>
2024-05-05 16:47:15 +0900

    feat: 영화 상페이지 조회

---
+++ cineffi/src/main/java/shinzo/cineffi/domain/dto/CrewListDTO.java
@@ -0,0 +1,15 @@
+package shinzo.cineffi.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+public class CrewListDTO {
+    private String name;
+    private byte[] profile;
+    private String job;
+    private String character;
+}
--- cineffi/src/main/java/shinzo/cineffi/movie/MovieService.java
+++ cineffi/src/main/java/shinzo/cineffi/movie/MovieService.java
@@ -400,5 +400,58 @@ public class MovieService {
         return boxOfficeMovieRepository.findAll();
     }
 
+
+//    public MovieDetailDTO findMovieDetails(Long movieId) {
+//        Movie movie = movieRepo.findById(movieId)
+//                .orElseThrow(() -> new CustomException(ErrorMsg.MOVIE_NOT_FOUND));
+//        Scrap scrap = scrapRepo.existsByMovieIdAndUserId()
+//
+//        List<CrewListDTO> crewList = getActorAndDorectorList(movieId);
+//        InMovieDetailDTO inMovieDetail = InMovieDetailDTO.builder()
+//                .movieId(movie.getId())
+//                .movieTitle(movie.getTitle())
+//                .releaseDate(movie.getReleaseDate())
+//                .poster(movie.getPoster())
+//                .originCountry(movie.getOriginCountry())
+//                .genre(movie.getGenreList().stream().map(MovieGenre::getGenre).map(Enum::name).collect(Collectors.toList()))
+//                .build();
+//
+//        return MovieDetailDTO.builder()
+//                .movie(inMovieDetail)
+//                .runtime(movie.getRuntime())
+//                .introduction(movie.getIntroduction())
+//                .cinephileAvgScore(movie.getAvgScore().getCinephileAvgScore())
+//                .levelAvgScore(movie.getAvgScore().getLevelAvgScore())
+//                .allAvgScore(movie.getAvgScore().getAllAvgScore())
+//                .myScore(null) //추후에 별점 가져오기 추가 (재영님 진행중)
+//                .isScrap(null) //추후 구현되고 가져오기
+//                .crewList(crewList)
+//                .build();
+//    }
+//
+//
+//    private List<CrewListDTO> getActorAndDorectorList(Long movieId) { //배우, 감독 가져오기
+//        List<CrewListDTO> actors = actorMovieRepo.findByMovieId(movieId)
+//                .stream()
+//                .map(am -> CrewListDTO.builder()
+//                        .name(am.getActor().getName())
+//                        .profile(am.getActor().getProfileImage())
+//                        .job("Actor")
+//                        .character(am.getCharacter())
+//                        .build())
+//                .collect(Collectors.toList());
+//
+//        Movie movie = movieRepo.findById(movieId).orElse(null);
+//        if (movie != null && movie.getDirector() != null) {
+//            actors.add(CrewListDTO.builder()
+//                    .name(movie.getDirector().getName())
+//                    .profile(movie.getDirector().getProfileImage())
+//                    .job("Director")
+//                    .character("")
+//                    .build());
+//        }
+//        return actors;
+//    }
 }
+
 
