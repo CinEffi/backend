03299bfeb1626b51f48de7820afe628e74215964
김민희 <gimminhui@gimminhuiui-MacBookPro.local>
2024-05-13 16:46:55 +0900

    하....

--- cineffi/src/main/java/shinzo/cineffi/config/MailConfig.java
+++ cineffi/src/main/java/shinzo/cineffi/config/MailConfig.java
@@ -1,33 +1,35 @@
-//package shinzo.cineffi.config;
-//
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.mail.javamail.JavaMailSenderImpl;
-//
-//import java.util.Properties;
-//
-//@Configuration
-//public class MailConfig {
-//
-//
-//    @Bean
-//    public JavaMailSenderImpl mailSender() {
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost("smtp.gmail.com"); // SMTP 호스트 설정
-//        mailSender.setPort(587); // SSL 포트 설정
-//
-//        mailSender.setUsername("skwd1012@gmail.com");
-//        mailSender.setPassword("tboxbeusjvbysajd");
-//
-//        Properties props = mailSender.getJavaMailProperties();
-//        props.put("mail.transport.protocol", "smtp"); // SMTP 프로토콜 사용
-//        props.put("mail.smtp.auth", "true"); // SMTP 인증 활성화
-//        props.put("mail.smtp.starttls.enable", "true"); // STARTTLS 활성화
-//        props.put("mail.smtp.starttls.required", "true"); // STARTTLS 필수
-//        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // SSL 소켓 팩토리 사용
-//        props.put("mail.smtp.socketFactory.port", "465"); // SSL 소켓 팩토리 포트
-//
-//        return mailSender;
-//    }
-//}
+package shinzo.cineffi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSenderImpl mailSender() {
+        // 시스템 속성 설정
+        System.setProperty("socksProxyHost", "krmp-proxy.9rum.cc");
+        System.setProperty("socksProxyPort", "3128");
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com"); // SMTP 호스트 설정
+        mailSender.setPort(587); // SMTP 포트 설정
+
+        mailSender.setUsername("skwd1012@gmail.com");
+        mailSender.setPassword("tboxbeusjvbysajd");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.socks.host", "krmp-proxy.9rumrum.cc"); // 프록시 호스트
+        props.put("mail.smtp.socks.port", "3128"); // 프록시 포트
+
+        return mailSender;
+    }
+}
