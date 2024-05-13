1840c0fb1ecce903e87e65156b414ab54b3f41a7
김민희 <gimminhui@gimminhuiui-MacBookPro.local>
2024-05-13 17:30:05 +0900

    프록시 4트

--- cineffi/src/main/java/shinzo/cineffi/TestController.java
+++ cineffi/src/main/java/shinzo/cineffi/TestController.java
@@ -14,44 +14,6 @@ public class TestController {
 
     @GetMapping("/api/test")
     public String test() {;
-        // 메일 서버 설정
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP 서버 주소
-        props.put("mail.smtp.port", "587"); // SMTP 포트 번호
-        props.put("mail.smtp.auth", "true"); // 인증 필요 여부
-        props.put("mail.smtp.starttls.enable", "true"); // TLS 사용 여부
-
-        // 프록시 설정
-        props.put("mail.smtp.proxy.host", "krmp-proxy.9rum.cc");
-        props.put("mail.smtp.proxy.port", "3128");
-
-        // 사용자 인증 정보
-        final String username = "skwd1012@gmail.com";
-        final String password = "tboxbeusjvbysajd";
-
-        // 세션 생성
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
-
-        try {
-            // 메시지 생성
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("skwd1012@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("radic7700@gmail.com"));
-            message.setSubject("Test Email");
-            message.setText("This is a test email sent through a proxy server.");
-
-            // 메일 전송
-            Transport.send(message);
-            System.out.println("Email sent successfully!");
-
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
 
         return "백엔드와 통신 성공! 이제 안심하라구!";
     }
--- cineffi/src/main/java/shinzo/cineffi/config/MailConfig.java
+++ cineffi/src/main/java/shinzo/cineffi/config/MailConfig.java
@@ -11,10 +11,6 @@ public class MailConfig {
 
     @Bean
     public JavaMailSenderImpl mailSender() {
-        // 시스템 속성 설정
-//        System.setProperty("socksProxyHost", "krmp-proxy.9rum.cc");
-//        System.setProperty("socksProxyPort", "3128");
-
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com"); // SMTP 호스트 설정
         mailSender.setPort(587); // SMTP 포트 설정
@@ -27,8 +23,8 @@ public class MailConfig {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.starttls.required", "true");
-        props.put("mail.smtp.socks.host", "krmp-proxy.9rum.cc"); // 프록시 호스트
-        props.put("mail.smtp.socks.port", "3128"); // 프록시 포트
+        props.put("mail.smtp.proxy.host", "krmp-proxy.9rum.cc"); // 프록시 호스트
+        props.put("mail.smtp.proxy.port", "3128"); // 프록시 포트
         props.put("mail.debug", "true");
 
         return mailSender;
