d493cf187b9b0e45d524fbe08b4f84275933e946
김민희 <gimminhui@gimminhuiui-MacBookPro.local>
2024-05-13 15:24:00 +0900

    이메일 프록시 설정 변경

--- cineffi/src/main/java/shinzo/cineffi/config/MailConfig.java
+++ cineffi/src/main/java/shinzo/cineffi/config/MailConfig.java
@@ -13,22 +13,18 @@ public class MailConfig {
 
     @Bean
     public JavaMailSenderImpl mailSender() {
-        // 시스템 속성으로 HTTP 프록시 설정
-        System.setProperty("http.proxyHost", "krmp-proxy.9rum.cc");
-        System.setProperty("http.proxyPort", "3128");
-
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com"); // SMTP 호스트 설정
-        mailSender.setPort(587); // SMTP 포트 설정
+        mailSender.setPort(465); // SSL 포트 설정
 
         mailSender.setUsername("skwd1012@gmail.com");
         mailSender.setPassword("tboxbeusjvbysajd");
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.transport.protocol", "smtps"); // SMTPS 프로토콜 사용
+        props.put("mail.smtps.auth", "true");
+        props.put("mail.smtps.starttls.enable", "true");
+        props.put("mail.smtps.starttls.required", "true");
 
         return mailSender;
     }
