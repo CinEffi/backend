a1f2cd0a69ef38cb83e333dafdd474689cc43864
김민희 <gimminhui@gimminhuiui-MacBookPro.local>
2024-05-14 14:03:11 +0900

    더 옛날로 원복

--- cineffi/src/main/java/shinzo/cineffi/config/SecurityConfig.java
+++ cineffi/src/main/java/shinzo/cineffi/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                                 "http://localhost:3003",
                                 "https://48fb-175-197-204-117.ngrok-free.app",
                                 "https://9e54-180-70-193-179.ngrok-free.app",
-                                "https://kd0fa487ebb45a.user-app.krampoline.com"));
+                                "https://k7f10638b4382a.user-app.krampoline.com/"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
                         configuration.setMaxAge(60L);
