6a0e0131ea3256c1860f4fb17f152f8bfba97986
Yeji <137299181+piczza@users.noreply.github.com>
2024-05-17 13:57:58 +0900

    fix : 검색 기능 향상

--- cineffi/src/main/java/shinzo/cineffi/Utils/CinEffiUtils.java
+++ cineffi/src/main/java/shinzo/cineffi/Utils/CinEffiUtils.java
@@ -8,6 +8,7 @@ public class CinEffiUtils {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static Float averageScore(Float sum, Integer count) {
+        if(sum == null || count == null) return null;
         return 0 < count ? Math.round((sum / count) * 10.0f) / 10.0f : null;
     }
 
