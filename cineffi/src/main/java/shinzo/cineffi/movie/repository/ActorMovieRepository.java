3b553ea781cfbfc137d955aebb900ba6cad84a5a
김민희 <gimminhui@gimminhuiui-MacBookPro.local>
2024-05-23 10:24:33 +0900

    chore: rollback #128

--- cineffi/src/main/java/shinzo/cineffi/movie/repository/ActorMovieRepository.java
+++ cineffi/src/main/java/shinzo/cineffi/movie/repository/ActorMovieRepository.java
@@ -1,5 +1,6 @@
 package shinzo.cineffi.movie.repository;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,6 @@ import java.util.List;
 public interface ActorMovieRepository extends JpaRepository<ActorMovie, Long> {
     @Query("SELECT am FROM ActorMovie am WHERE am.movie.id = :movieId ORDER BY am.orders ASC")
     List<ActorMovie> findByMovieId(Long movieId, Pageable pageable);
+
+    boolean existsByMovieIdAndActorId(Long movieId, Long actorId);
 }
