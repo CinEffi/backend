53051cc15fcd2cbf7d1016453354dd175a40b0a2
minnie <radic7700@gmail.com>
2025-02-21 13:03:44 +0900

    feat: health check 수정

--- src/main/java/shinzo/cineffi/TestController.java
+++ src/main/java/shinzo/cineffi/TestController.java
@@ -1,21 +1,17 @@
 package shinzo.cineffi;
 
-import jakarta.mail.*;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Properties;
 
 @RestController
 public class TestController {
 
     @GetMapping("/api/health")
     @CrossOrigin
-    public String test() {
+    public ResponseEntity<?> test() {
 
-        return "백엔드와 통신 성공! 이제 안심하라구!";
+        return ResponseEntity.ok().build();
     }
 }
