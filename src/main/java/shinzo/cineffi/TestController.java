ab3b4df58bd1b9b41f1fe2c5d93e01bbe15e03cb
minnie <radic7700@gmail.com>
2025-02-27 21:44:53 +0900

    feat: health check 경로 변경

--- src/main/java/shinzo/cineffi/TestController.java
+++ src/main/java/shinzo/cineffi/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/api/health")
+    @GetMapping("/")
     @CrossOrigin
     public ResponseEntity<?> test() {
 
